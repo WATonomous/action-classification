a8e6b454676a46b302ececd299b6fe07f765f617
Ben Zhang <ben.zhang@uwaterloo.ca>
2021-06-14 18:47:15 -0400

    Print system info in the retina-job

--- retina-job.sh
+++ retina-job.sh
@@ -5,9 +5,17 @@
 #SBATCH --time=0-00:15:00
 #SBATCH --output=logs/pytorch-test-%N-%j.out
 
+# This script assumes that the data is already downloaded and preprocessed
+# See the 3D-RetinaNet repo for instructions
+
+# print system info
+./sys-info.sh
+
+VENV_DIR=${SLURM_TMRDIR:-/tmp}/venv
+
 module load python/3
-virtualenv --no-download $SLURM_TMPDIR/venv
-source $SLURM_TMPDIR/venv/bin/activate
+virtualenv --no-download $VENV_DIR
+source $VENV_DIR/bin/activate
 pip install --no-index  torch torchvision tensorboardx numpy scipy pandas matplotlib
 
 python 3D-RetinaNet/main.py ./data/ ./output/ ./data/kinetics-pt/ --MODE=train --ARCH=resnet50 --MODEL_TYPE=I3D --DATASET=road --TRAIN_SUBSETS=train_1 --VAL_SUBSETS=val_1 --SEQ_LEN=8 --TEST_SEQ_LEN=8 --BATCH_SIZE=1 --LR=0.00245 --MILESTONES=6,8 --MAX_EPOCHS=10
