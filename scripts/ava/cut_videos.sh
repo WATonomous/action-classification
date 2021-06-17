9269b82021548de41fbd70832f6b5b240954eae9
Rowan Dempster <r2dempst@uwaterloo.ca>
2021-06-17 17:22:38 -0400

    job arrayed long running ava data scripts

--- scripts/ava/cut_videos.sh
+++ scripts/ava/cut_videos.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 
-#SBATCH --cpus-per-task=1
-#SBATCH --mem=12G
-#SBATCH --time=0-02:00:00
-#SBATCH --output=logs/ava-cut-videos-%j.out
-
-IN_DATA_DIR="../../../../data/ava/videos/"
-OUT_DATA_DIR="../../../../data/ava/videos_15min_job/"
+IN_DATA_DIR="../../../../data/ava/videos"
+OUT_DATA_DIR="../../../../data/ava/videos_15min"
 
 if [[ ! -d "${OUT_DATA_DIR}" ]]; then
   echo "${OUT_DATA_DIR} doesn't exist. Creating it.";
   mkdir -p ${OUT_DATA_DIR}
 fi
 
-for video in $(ls -A1 -U ${IN_DATA_DIR}/*)
+FILES=($(ls -A1 -U ${IN_DATA_DIR}/*))
+if [[ "$#" < 1 ]]; then
+  echo "Cutting all files"
+else 
+  echo "Cutting file $1"
+  FILES=("${FILES[$1]}")
+fi
+for video in $FILES
 do
   out_name="${OUT_DATA_DIR}/${video##*/}"
   if [ ! -f "${out_name}" ]; then
+    echo "Calling ffmpeg to output ${out_name}"
     ffmpeg -ss 900 -t 901 -i "${video}" "${out_name}"
+  else
+    echo "Output ${out_name} already exists, skipping"
   fi
 done
--- scripts/ava/download_videos.sh
+++ scripts/ava/download_videos.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+#SBATCH --time=0:10:00
+#SBATCH --array=0-300
+#SBATCH --output=logs/ava-download-videos-%j.log
+
 DATA_DIR="../../../../data/ava/videos/"
 
 if [[ ! -d "${DATA_DIR}" ]]; then
@@ -6,8 +11,12 @@ if [[ ! -d "${DATA_DIR}" ]]; then
 fi
 
 wget https://s3.amazonaws.com/ava-dataset/annotations/ava_file_names_trainval_v2.1.txt
+LINKS=($(cat ava_file_names_trainval_v2.1.txt))
+if [ -n $SLURM_ARRAY_TASK_ID ]; then
+  LINK=$(sed -n "${SLURM_ARRAY_TASK_ID}p" ava_file_names_trainval_v2.1.txt)
+fi
 
-for line in $(cat ava_file_names_trainval_v2.1.txt)
+for line in $LINKS
 do
   wget https://s3.amazonaws.com/ava-dataset/trainval/$line -P ${DATA_DIR}
 done
